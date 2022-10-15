d60728b8bdbf148eed28813286a55a3bc0e838cc
Michael Cooper <cooper@w3.org>
2022-10-15 08:23:42 -0400

    refactor script

--- script/display-registry.js
+++ script/display-registry.js
@@ -1,49 +1,39 @@
-// script adapted from https://stackoverflow.com/questions/5180382/convert-json-data-to-a-html-table#answer-21065846
+var _tr_ = document.createElement('tr'),
+_td_ = document.createElement('td');
 
-var _table_ = document.createElement('table'),
-  _tr_ = document.createElement('tr'),
-  _th_ = document.createElement('th'),
-  _td_ = document.createElement('td');
-
-// Builds the HTML Table out of myList json data from Ivy restful service.
-function buildHtmlTable(arr) {
-  var table = _table_.cloneNode(false),
-    columns = addAllColumnHeaders(arr, table);
-  for (var i = 0, maxi = arr.length; i < maxi; ++i) {
+function buildHtmlTable(id, arr) {
+  var tbody = document.getElementById(id).querySelector("tbody");
+  for (var i = 0; i < arr.length;++ i) {
     var tr = _tr_.cloneNode(false);
-    for (var j = 0, maxj = columns.length; j < maxj; ++j) {
-      var td = _td_.cloneNode(false);
-      var cellValue = arr[i][columns[j]];
-      td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
-      tr.appendChild(td);
-    }
-    table.appendChild(tr);
+    var td = _td_.cloneNode(false);
+    
+    var img = document.createElement('img');
+    img.setAttribute("src", "reference-images/" + arr[i]["bciav"]);
+    img.setAttribute("alt", "");
+    img.setAttribute("loading", "lazy");
+    
+    td.appendChild(img);
+    tr.appendChild(td);
+    
+    td = _td_.cloneNode(false);
+    td.appendChild(document.createTextNode(arr[i]["bciav"] || ''));
+    tr.appendChild(td);
+
+    td = _td_.cloneNode(false);
+    td.appendChild(document.createTextNode(arr[i]["english"] || ''));
+    tr.appendChild(td);
+
+    td = _td_.cloneNode(false);
+    td.appendChild(document.createTextNode(arr[i]["derivationExplanation"] || ''));
+    tr.appendChild(td);
+
+    tbody.appendChild(tr);
   }
-  return table;
+  return tbody;
 }
 
-// Adds a header row to the table and returns the set of columns.
-// Need to do union of keys from all records as some records may not contain
-// all records
-function addAllColumnHeaders(arr, table) {
-  var columnSet = [],
-    tr = _tr_.cloneNode(false);
-  for (var i = 0, l = arr.length; i < l; i++) {
-    for (var key in arr[i]) {
-      if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1) {
-        columnSet.push(key);
-        var th = _th_.cloneNode(false);
-        th.appendChild(document.createTextNode(key));
-        tr.appendChild(th);
-      }
-    }
-  }
-  table.appendChild(tr);
-  return columnSet;
-}
-
-function addTableToDoc(id, uri) {
+function fillTable(id, uri) {
   fetch(uri).then(response => {
-     return response.json();
-  }).then(data => document.getElementById(id).appendChild(buildHtmlTable(data)));
+    return response.json();
+  }).then(data => buildHtmlTable(id, data));
 }
